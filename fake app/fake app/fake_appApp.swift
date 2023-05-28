cd79e4a81e51464a0d4ba7c70c1e19eb12d46601
Catravine <caroline@carolinecourtney.com>
2023-05-28 17:19:44 -0400

    first commit

---
+++ fake app/fake app/fake_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  fake_appApp.swift
+//  fake app
+//
+//  Created by Caroline Courtney on 5/28/23.
+//
+
+import SwiftUI
+
+@main
+struct fake_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
