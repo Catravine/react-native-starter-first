3877283756a0213cc74a96e018599c014e62cf1e
Catravine <caroline@carolinecourtney.com>
2023-06-04 15:25:11 -0400

    passwoard validation exercise

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet, TextInput } from 'react-native'
 
+const MIN_LENGTH = 5;
+
 const TextScreen = () => {
   const [name, setName] = useState('')
 
   return (
     <View>
-      <Text>Enter Name:</Text>
+      <Text>Enter Password:</Text>
       <TextInput 
         style={styles.input} 
         autoCapitalize='none' // stops iOS from messing with this
@@ -14,7 +16,7 @@ const TextScreen = () => {
         value={name}
         onChangeText={(newValue) => setName(newValue)}
       />
-      <Text>My name is {name}</Text>
+      {name.length < MIN_LENGTH ? (<Text>Password must be at least {MIN_LENGTH} characters</Text>) : null}
     </View>
   );
 };
